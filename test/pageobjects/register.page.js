4b59fbfebbc1586cc4bc0a0e97e3e6434e5efc5c
RobH502 <robert.hayes@auticon.us>
2020-05-21 13:15:30 -0700

    Updated Register file

---
+++ test/pageobjects/register.page.js
@@ -0,0 +1,38 @@
+import Page from './page'
+
+class Register extends Page {
+
+    /**
+    * define elements
+    */
+
+
+    get registerLink() { return $('//a[contains(@href, "/register")]'); }
+    get firstNameField()  {return $('input[@name="firstName"]'); }
+
+    /**
+     * define or overwrite page methods
+     */
+    open () {
+        super.open('')       //this will append `contact-us` to the baseUrl to form complete URL
+        browser.pause(2000);
+    }
+
+    clickRegister() {
+      this.registerLink.click();
+    }
+
+
+/*
+    waitForAccountPageToLoad () {
+      if(!this.accountMessage.isDisplayed()){
+        this.accountMessage.waitForDisplayed(5000);
+      }
+    }
+    getUserInfo (){
+      return this.accountMessage.getText();
+    }
+    */
+}
+
+export default new Register()
